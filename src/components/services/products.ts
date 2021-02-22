f8af309312d00f1250626f85434e06a38467ef97
Lari Löfman <larwazor@gmail.com>
2021-02-23 01:43:56 +0200

    modify apiUrl

--- src/components/services/products.ts
+++ src/components/services/products.ts
@@ -2,6 +2,6 @@ import axios from 'axios';
 import { apiUrl } from '../../data.json';
 
 export const getProductsByCategory = async (category: string) => {
-    const response = await axios.get(`${apiUrl}products/${category.toLowerCase()}`);
+    const response = await axios.get(`${apiUrl}/products/${category.toLowerCase()}`);
     return response.data;
 };
