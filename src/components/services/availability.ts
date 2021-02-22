4fd436c979fe235e53e51d1afdd3626116a8ae52
Lari Löfman <larwazor@gmail.com>
2021-02-23 01:27:59 +0200

    add api url

--- src/components/services/availability.ts
+++ src/components/services/availability.ts
@@ -1,6 +1,7 @@
 import axios from 'axios';
+import { apiUrl } from '../../data.json';
 
 export const getAvailabilityByManufacturer = async (manufacturer: string) => {
-    const response = await axios.get(`availability/${manufacturer}`);
+    const response = await axios.get(`${apiUrl}availability/${manufacturer}`);
     return response.data;
 };
--- src/components/services/products.ts
+++ src/components/services/products.ts
@@ -1,6 +1,7 @@
 import axios from 'axios';
+import { apiUrl } from '../../data.json';
 
 export const getProductsByCategory = async (category: string) => {
-    const response = await axios.get(`products/${category.toLowerCase()}`);
+    const response = await axios.get(`${apiUrl}products/${category.toLowerCase()}`);
     return response.data;
 };
