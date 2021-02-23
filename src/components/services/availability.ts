54ec2fadec8bf55f5c16656219441185d0fc06d3
Lari Löfman <larwazor@gmail.com>
2021-02-23 02:17:09 +0200

    cors crap

--- src/components/services/availability.ts
+++ src/components/services/availability.ts
@@ -3,7 +3,7 @@ import { apiUrl } from '../../data.json';
 
 export const getAvailabilityByManufacturer = async (manufacturer: string) => {
     const response = await axios.get(`${apiUrl}/availability/${manufacturer}`, {
-        headers: { 'referer': 'https://warehouse-listing.herokuapp.com/' }
+        headers: { 'Origin': 'https://warehouse-listing.herokuapp.com/' }
     });
     return response.data;
 };
--- src/components/services/products.ts
+++ src/components/services/products.ts
@@ -4,7 +4,7 @@ import { apiUrl } from '../../data.json';
 export const getProductsByCategory = async (category: string) => {
     console.log(`${apiUrl}/products/${category.toLowerCase()}`);
     const response = await axios.get(`${apiUrl}/products/${category.toLowerCase()}`, {
-        headers: { 'referer': 'https://warehouse-listing.herokuapp.com/' }
+        headers: { 'Origin': 'https://warehouse-listing.herokuapp.com/' }
     });
     return response.data;
 };
