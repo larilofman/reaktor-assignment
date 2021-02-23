0d2ec9e0454c83bfb92ae2d8ae59fe8302fa2df4
Lari Löfman <larwazor@gmail.com>
2021-02-23 02:02:22 +0200

    cors crap

--- src/components/services/products.ts
+++ src/components/services/products.ts
@@ -2,6 +2,7 @@ import axios from 'axios';
 import { apiUrl } from '../../data.json';
 
 export const getProductsByCategory = async (category: string) => {
+    console.log(`${apiUrl}/products/${category.toLowerCase()}`);
     const response = await axios.get(`${apiUrl}/products/${category.toLowerCase()}`);
     return response.data;
 };
