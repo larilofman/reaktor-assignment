a5318f5690e72bde28f98b8b59c55cffd92f00b3
Lari Löfman <larwazor@gmail.com>
2021-02-23 03:26:04 +0200

    add proxy

--- src/components/services/products.ts
+++ src/components/services/products.ts
@@ -2,6 +2,7 @@ import axios from 'axios';
 import { apiUrl, proxy } from '../../data.json';
 
 export const getProductsByCategory = async (category: string) => {
+    console.log(`${proxy}/${apiUrl}/products/${category.toLowerCase()}`);
     const response = await axios.get(`${proxy}/${apiUrl}/products/${category.toLowerCase()}`);
     return response.data;
 };
