4999aae8dbe3e21c6ea8ea32d40553aa31df3318
Lari Löfman <larwazor@gmail.com>
2021-02-23 02:23:28 +0200

    cors crap

--- src/components/services/availability.ts
+++ src/components/services/availability.ts
@@ -1,9 +1,11 @@
 import axios from 'axios';
 import { apiUrl } from '../../data.json';
 
+export const config = {
+    headers: { 'Access-Control-Allow-Origin': '*' }
+};
+
 export const getAvailabilityByManufacturer = async (manufacturer: string) => {
-    const response = await axios.get(`${apiUrl}/availability/${manufacturer}`, {
-        headers: { 'Origin': 'https://warehouse-listing.herokuapp.com/' }
-    });
+    const response = await axios.get(`${apiUrl}/availability/${manufacturer}`, config);
     return response.data;
 };
--- src/components/services/products.ts
+++ src/components/services/products.ts
@@ -1,10 +1,9 @@
 import axios from 'axios';
 import { apiUrl } from '../../data.json';
+import { config } from './availability';
 
 export const getProductsByCategory = async (category: string) => {
     console.log(`${apiUrl}/products/${category.toLowerCase()}`);
-    const response = await axios.get(`${apiUrl}/products/${category.toLowerCase()}`, {
-        headers: { 'Origin': 'https://warehouse-listing.herokuapp.com/' }
-    });
+    const response = await axios.get(`${apiUrl}/products/${category.toLowerCase()}`, config);
     return response.data;
 };
